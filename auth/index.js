e02403bbfa515453a12ca7f0647045fa4cbd77a8
finne132 <finne132@umn.edu>
2018-07-10 19:24:00 -0500

    added basic authfile for passport

---
+++ auth/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var config = require('./config');
+var passport = require('passport');
+var loggern= require('./logger');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+// Authentication code 
+var init = function(){
+ // auth code goes here 
+}
+	
+module.exports = init();
