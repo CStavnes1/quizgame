fd1dc125494216492e4eb194eeb9cacd6b124ea6
finne132 <finne132@umn.edu>
2018-07-10 19:26:07 -0500

    pasted in passport auth template code

--- auth/index.js
+++ auth/index.js
@@ -9,6 +9,42 @@ var User = require('./models/user');
 // Authentication code 
 var init = function(){
  // auth code goes here 
+
+ // send to and check user instances from session with serializeUser and deserializeUser
+
+ // serialize
+ passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// de-serialize
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// Insert the passport LocalStrategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: new RegExp(username, 'i') }, function(err, user) {
+      if (err) { return done(err); }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+
+      user.validatePassword(password, function(err, isMatch) {
+            if (err) { return done(err); }
+            if (!isMatch){
+                return done(null, false, { message: 'Incorrect username or password.' });
+            }
+            return done(null, user);
+      });
+    });
+  }
+));
+return passport;
 }
 	
 module.exports = init();
